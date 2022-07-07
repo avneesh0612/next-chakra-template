088177ed4753114f56f0df16015497ee76fecf55
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2022-07-07 17:51:38 +0530

    feat: update template

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,28 +21,15 @@ module.exports = {
         sourceType: 'module',
     },
     rules: {
-        // Possible errors
         'no-console': 'warn',
-        // Best practices
         'dot-notation': 'error',
         'no-else-return': 'error',
         'no-floating-decimal': 'error',
         'no-sequences': 'error',
-        // Stylistic
         'array-bracket-spacing': 'error',
         'computed-property-spacing': ['error', 'never'],
         curly: 'error',
-        'max-len': [
-            'error',
-            {
-                comments: 120,
-                code: 581,
-                ignorePattern: '^import\\W.*',
-                ignoreTrailingComments: true,
-            },
-        ],
         'no-lonely-if': 'error',
-        // 'no-nested-ternary': 'error',
         'no-unneeded-ternary': 'error',
         'one-var-declaration-per-line': 'error',
         quotes: [
@@ -53,16 +40,11 @@ module.exports = {
                 avoidEscape: true,
             },
         ],
-
-        // ES6
         'array-callback-return': 'off',
         'prefer-const': 'error',
-        // Imports
         'import/prefer-default-export': 'off',
         'no-unused-expressions': 'off',
-        // Using a lot of hasOwnProperty
         'no-prototype-builtins': 'off',
-        // REACT
         'react/jsx-uses-react': 'off',
         'react/react-in-jsx-scope': 'off',
         'jsx-a11y/href-no-hash': [0],
@@ -74,13 +56,14 @@ module.exports = {
                 checkAliases: true,
             },
         ],
-
         'react-hooks/rules-of-hooks': 'error',
         'react-hooks/exhaustive-deps': 0,
-        // Prettier
-        // eslint looks for the prettier config at the top level of the package/app
-        // but the config lives in the `config/` directory. Passing the config here
-        // to get around this.
+        'react/jsx-sort-props': [
+            'error',
+            {
+                ignoreCase: true,
+            },
+        ],
         'prettier/prettier': ['error', prettierConfig],
     },
     settings: {
